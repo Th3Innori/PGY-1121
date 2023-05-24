92ea893b26e23f95e9509cfddf366b7c62d4978d
Amaru <134552877+Th3Innori@users.noreply.github.com>
2023-05-24 16:43:22 -0400

    Datos Solicitados

---
+++ CLASE_12/datos.py
@@ -0,0 +1,9 @@
+# Ejemplo uso de GITHUB
+print("ingreso de datos")
+print("------------------ \n")
+nom = input("ingrese su nombre: ")
+edad = input("ingrese su edad: ")
+print("-------------------")
+print(f"su nombre es {nom}")
+print(f"su edad es {edad}")
+print("programa finalizado..")
